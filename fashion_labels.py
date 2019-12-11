bd42de534a225a39dbdcf2a4eef49cae4a848b7e
Navneet Sharma <navtheunstoppable@gmail.com>
2019-12-11 14:18:45 -0800

    added dropout layers and gitignore file to avoid tracking .h5 files

--- fashion_labels.py
+++ fashion_labels.py
@@ -21,18 +21,22 @@ for i in tqdm(range(train_data.shape[0])):
     train_images.append(img)
 X = np.array(train_images)
 y = np.array(train_data.drop(['ImageId'],axis = 1))
-X_train, y_train,X_test, y_test = train_test_split(X,y, random_state = 42, test_size = 0.2)
+X_train, X_test,y_train, y_test = train_test_split(X,y, random_state = 42, test_size = 0.2)
 
 #define the model
 model = models.Sequential()
 model.add(layers.Conv2D(32,(3,3),activation = 'relu', input_shape = (112,112,3)))
 model.add(layers.MaxPooling2D(2,2))
+model.add(layers.Dropout(0.2))
 model.add(layers.Conv2D(64,(3,3), activation = 'relu'))
 model.add(layers.MaxPooling2D(2,2))
+model.add(layers.Dropout(0.2))
 model.add(layers.Conv2D(128,(3,3), activation = 'relu'))
 model.add(layers.MaxPooling2D(2,2))
+model.add(layers.Dropout(0.2))
 model.add(layers.Flatten())
 model.add(layers.Dense(1024, activation = 'relu'))
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(46,activation = 'sigmoid'))
 
 #compile the model
@@ -53,12 +57,20 @@ with open('fashion_labels_model.json','w') as json_file:
 #plot loss curves during training
 loss = history.history['loss']
 val_loss = history.history['val_loss']
+acc = history.history['acc']
+val_acc = history.history['val_acc']
 
-epochs = range(1,len(loss)+1)
+epochs = range(1,len(acc)+1)
 
 plt.plot(epochs,loss,'ro',label = 'Training Loss')
 plt.plot(epochs, val_loss, 'r', label = 'Validation Loss')
 plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+
+plt.plot(epochs,acc,'bo',label = 'Training Accuracy')
+plt.plot(epochs, val_acc, 'b', label = 'Validation Accuracy')
+plt.title('Training and validation Accuracy')
 plt.legend()
 plt.show()
 
@@ -67,6 +79,7 @@ img = image.load_img('test.jpg',target_size=(112,112,3))
 img = image.img_to_array(img)
 img = img/255
 
+#print top 3 labels predicted by the model
 labels = np.array(train_data.columns[1:])
 predictions = model.predict(img.reshape(1,112,112,3))
 top_3 = np.argsort(predictions[0])[:-4:-1]
